63c118e8bc193f991ef0c3b6554ef2503e0d86ca
Rachel Alpert <ambitious.fishus@gmail.com>
2016-11-14 17:33:03 -0500

    Rename 'utility_functions.js' to 'utility.js'

--- commands/axe.js
+++ commands/axe.js
@@ -2,7 +2,7 @@ module.exports = {
   description: `AXE ME A QUESTION`,
   process: function(bot, msg, args) {
     let magicAxeBallAnswers = require('../responses').magicAxeBallAnswers;
-    let i = require('../utility_functions').randomNumber(magicAxeBallAnswers.length);
+    let i = require('../utility').randomNumber(magicAxeBallAnswers.length);
 
     if (msg.content.split(' ').length > 1) {
       msg.channel.sendMessage(magicAxeBallAnswers[i]);
--- commands/dota.js
+++ commands/dota.js
@@ -2,7 +2,7 @@ module.exports = {
   description: `LET ME TELL YOU ABOUT DOTA2`,
   process: function(bot, msg, args) {
     let dotaFeelings = require('../responses').dotaFeelings;
-    let i = require('../utility_functions').randomNumber(dotaFeelings.length);
+    let i = require('../utility').randomNumber(dotaFeelings.length);
     msg.channel.sendMessage(dotaFeelings[i]);
   }
 }
