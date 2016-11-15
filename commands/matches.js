0c90adbacec4edd0d100c5e1c0f5b8143ef9b992
Rachel Alpert <ambitious.fishus@gmail.com>
2016-11-15 18:31:57 -0500

    Fix bug that crashes bot if you send a bad steamid; disable preview for Dotabuff link

--- commands/matches.js
+++ commands/matches.js
@@ -22,21 +22,28 @@ module.exports = {
       else {
         let info = JSON.parse(data);
         let steamId = info.steamid;
-        request.get(`https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=${steamDevKey}&account_id=${steamId}`, (err, response, body) => {
-            console.log(`Steam response - ${response.statusCode}`);
-            if (err) {
-              console.log(err);
-            }
-            else {
-              let parsedData = JSON.parse(body);
-              let results = parsedData.result.matches;
-              let string = '';
-              for (let i = 0; i < 5; i++) {
-                string += `http://www.dotabuff.com/matches/${results[i].match_id}\n`;
+        try {
+          request.get(`https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=${steamDevKey}&account_id=${steamId}`, (err, response, body) => {
+              console.log(`Steam response - ${response.statusCode}`);
+              if (err) {
+                console.log(err);
               }
-              msg.channel.sendMessage(string);
-            }
-        }); 
+              else {
+                let parsedData = JSON.parse(body);
+                let results = parsedData.result.matches;
+                let string = '';
+                for (let i = 0; i < 5; i++) {
+                  string += `<http://www.dotabuff.com/matches/${results[i].match_id}>\n`;
+                }
+                string += `*ARE THESE YOUR MATCHES? IF NOT, MAKE SURE YOU **!register** THE RIGHT STEAMID!*`;
+                msg.channel.sendMessage(string);
+              }
+          }); 
+        }
+        catch (e) {
+          msg.reply(`AXE COULDN'T GET YOUR MATCHES!`);
+          console.log(`Encountered error -- ${e} -- when attempting to retrieve matches for ${msg.author}`);
+        }
       }
     });
   }
