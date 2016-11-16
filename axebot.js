0f78c9bccea4c4c9cbeed00cc9bf84de50ad3215
Rachel Alpert <ambitious.fishus@gmail.com>
2016-11-15 19:56:06 -0500

    Stop recording commands and bot chatter in markov.txt

--- axebot.js
+++ axebot.js
@@ -21,34 +21,37 @@ bot.on('error', e => console.error(e));
 bot.login(secret.botToken);
 
 function parseMessages(msg) {
-  markovListener.write(`${msg.content}\n`);
+  if (!msg.author.bot) {
+    if (msg.content[0] === Config.cmdPrefix) {
+      let command = msg.content.split(' ')[0].substring(1).toLowerCase();
+      let args = msg.content.substring(command.length+2);
 
-  if (!msg.author.bot && (msg.content[0] === Config.cmdPrefix)) {
-    let command = msg.content.split(' ')[0].substring(1).toLowerCase();
-    let args = msg.content.substring(command.length+2);
+      console.log(`Processing ${command} command from ${msg.author}.`);
 
-    console.log(`Processing ${command} command from ${msg.author}.`);
+      let cmd = commands[command];
 
-    let cmd = commands[command];
-
-    if (command == 'help') {
-      let str = '';
-      for (key in commands) {
-        str += `**${Config.cmdPrefix}${key}** -- ${commands[key].description}\n`;
+      if (command == 'help') {
+        let str = '';
+        for (key in commands) {
+          str += `**${Config.cmdPrefix}${key}** -- ${commands[key].description}\n`;
+        }
+        msg.channel.sendMessage(str);
       }
-      msg.channel.sendMessage(str);
-    }
-    else if (cmd) {
-      try {
-        cmd.process(bot, msg, args);
+      else if (cmd) {
+        try {
+          cmd.process(bot, msg, args);
+        }
+        catch (e) {
+          msg.reply(`*I'm sorry, I can't do that, Dave...*`);
+          console.log(`There was an error -- ${e} -- processing ${command} command from ${msg.author}.`);
+        }
       }
-      catch (e) {
-        msg.reply(`*I'm sorry, I can't do that, Dave...*`);
-        console.log(`There was an error -- ${e} -- processing ${command} command from ${msg.author}.`);
+      else {
+        msg.reply(`AXE CAN'T DO THAT! TRY **!help** TO SEE WHAT I CAN DO`);
       }
     }
     else {
-      msg.reply(`AXE CAN'T DO THAT! TRY **!help** TO SEE WHAT I CAN DO`);
+      markovListener.write(`${msg.content}\n`);
     }
   }
 }
